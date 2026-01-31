7bb502079953b043c22bfc35502fc2a6d36ec99b
irgmood-beep <irgmood@gmail.com>
2026-01-31 19:01:27 +0330

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+define('API_KEY', '8137720844:AAFzS8tRGmMS6p17oMLydSwAHZtEuQRq084'); 
+$admin = '6419827038'; 
+
+// مشخصات جدید دیتابیس ری‌ل‌وی شما
+$databaseName = 'railway'; 
+$databaseUser = 'root'; 
+$databasePass = 'bYFHOMNYfyaZbqgBssOJRarEjZDzmmgN'; 
+$databaseHost = 'shinkansen.proxy.rlwy.net'; 
+$databasePort = '48624';
+
+// آدرس پروژه
+$web = 'https://uploaderbot-production-abd4.up.railway.app/';
