2776d88a98052eb8a1d5429dfa229b7eb2b226ea
irgmood-beep <irgmood@gmail.com>
2026-01-31 19:07:09 +0330

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+$API_KEY = '8137720844:AAFzS8tRGmMS6p17oMLydSwAHZtEuQRq084';
+define('API_KEY', $API_KEY);
+
+$admin = '6419827038';
+
+// اطلاعات دیتابیس - پورت 48624 مخصوص اتصال خارجی است
+$databaseHost = 'shinkansen.proxy.rlwy.net';
+$databasePort = 48624;
+$databaseUser = 'root';
+$databasePass = 'bYFHOMNYfyaZbqgBssOJRarEjZDzmmgN';
+$databaseName = 'railway';
+
+$web = 'https://uploaderbot-production-abd4.up.railway.app/';
